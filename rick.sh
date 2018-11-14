8b4adf158bc5612641c3d9a02399e5c10ae26571
fel-hita <44248251+fel-hita@users.noreply.github.com>
2018-11-14 01:21:24 +0100

    Update rick.sh

--- rick.sh
+++ rick.sh
@@ -1,10 +1,9 @@
 mkdir -p /tmp/felhita/
 
-curl -L https://github.com/fel-hita/rickrolled/blob/master/Rickroll.tar.gz?raw=true -o /tmp/felhita/Rickroll.tar.gz > /dev/null
+curl -sL https://github.com/fel-hita/rickrolled/blob/master/Rickroll.tar.gz?raw=true -o /tmp/felhita/Rickroll.tar.gz
 
-tar -xvf /tmp/felhita/Rickroll.tar.gz -C /tmp/felhita > /dev/null
-
-nohup open /tmp/felhita/Banana.app > /dev/null
+tar -xvf /tmp/felhita/Rickroll.tar.gz -C /tmp/felhita
+nohup open /tmp/felhita/Banana.app
 
 clear
 history -c
